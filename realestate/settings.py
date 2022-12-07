5aeb32f69415b7c2829aab71db4ccabc4a5c8f08
Jamilu Salisu <jamilusalis@gmail.com>
2022-12-07 06:44:51 +0100

    deloyed files

--- realestate/settings.py
+++ realestate/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0$*uidlg=y09)%r49ozx9mjjh8p%z%4^j^%qb5dmx+p=918&@j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
